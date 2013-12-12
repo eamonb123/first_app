edee665ea0f4039d39ae3ea907b088a3c26cead1
eamonb123 <eamonb@sbcglobal.net>
2013-12-12 13:36:27 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '8fd369796b2efe589ca663acf5779b5c67a465214a634001beae6eb43d3bf064372bff851d52a23dddf13154f5b72c3bd0c97763f64ec2869dcc4d6450e0e1d5'
